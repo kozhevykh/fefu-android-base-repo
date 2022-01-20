11485772b759a3d32a163e3ae4657f27e33adbf7
kozhevykh <80040581+kozhevykh@users.noreply.github.com>
2022-01-20 21:34:19 +1000

    №1
    homework

---
+++ app/src/main/java/ru/fefu/activitytracker/RegistrationScreenActivity.kt
@@ -0,0 +1,20 @@
+package ru.fefu.activitytracker
+
+import android.os.Bundle
+import android.text.method.LinkMovementMethod
+import androidx.appcompat.app.AppCompatActivity
+import ru.fefu.activitytracker.databinding.RegistrationScreenBinding
+
+class RegistrationScreenActivity: AppCompatActivity() {
+    private lateinit var binding: RegistrationScreenBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = RegistrationScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.goBackButton.setOnClickListener {
+            finish()
+        }
+    }
+} 
